a205d5e21471121dd1b89856073604a7267987ab
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-02-27 09:17:40 -0700

    Add tests and working for code for initialize, and add preference

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,3 +1,21 @@
 class Visitor
-  
+  attr_reader :name, 
+              :height, 
+              :spending_money,
+              :preferences
+
+  def initialize(name, height, spending_money)
+    @name = name
+    @height = height
+    @spending_money = adjust_spending_money(spending_money)
+    @preferences = []
+  end
+
+  def adjust_spending_money(spending_money)
+    spending_money[1..-1].to_i
+  end
+
+  def add_preference(preference)
+    @preferences.push(preference)
+  end
 end
