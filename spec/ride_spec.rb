4abee18130ae7a0e92d56a06b3f507d04e1366c1
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-02-27 09:25:41 -0700

    Add tests for initializing Ride class

---
+++ spec/ride_spec.rb
@@ -0,0 +1,26 @@
+require 'rspec'
+require './lib/visitor'
+require './lib/rise'
+
+RSpec.describe Ride do
+  before(:each) do
+    @ride1 = Ride.new({ name: 'Carousel', 
+                        min_height: 24, 
+                        admission_fee: 1, 
+                        excitement: :gentle 
+                        })
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
+  end
+
+  describe '#initialize' do
+    it 'exists and has attributes' do
+      expect(@ride1).to be_a(Ride)
+      expect(@ride1.name).to eq('Carousel')
+      expect(@ride1.min_height).to eq(24)
+      expect(@ride1.admission_fee).to eq(1)
+      expect(@ride1.excitement).to eq(:gentle)
+    end
+  end
+end
