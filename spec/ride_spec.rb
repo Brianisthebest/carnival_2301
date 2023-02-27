4fd654306c48495548622a2916363c607bd88103
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-02-27 09:34:16 -0700

    Add tests for rider_log

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -21,6 +21,18 @@ RSpec.describe Ride do
       expect(@ride1.min_height).to eq(24)
       expect(@ride1.admission_fee).to eq(1)
       expect(@ride1.excitement).to eq(:gentle)
+      expect(@ride1.total_revenue).to eq(0)
+    end
+  end
+
+  describe '#rider_log' do
+    it 'keeps tracks of riders boarded' do
+      @visitor1.add_preference(:gentle)
+      @visitor2.add_preference(:gentle)
+      @ride1.board_rider(@visitor1)
+      @ride1.board_rider(@visitor2)
+      @ride1.board_rider(@visitor1)
+      expect(@ride1.rider_log).to eq({@visitor1 => 2, @visitor2 => 1})
     end
   end
 end
